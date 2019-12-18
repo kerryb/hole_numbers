d0ef3ee865de4c43dbaa2d21757701a863e72876
Kerry Buckley <kerryjbuckley@gmail.com>
2019-12-18 14:58:37 +0000

    New project with README

---
+++ lib/hole_numbers.ex
@@ -0,0 +1,18 @@
+defmodule HoleNumbers do
+  @moduledoc """
+  Documentation for HoleNumbers.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HoleNumbers.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hole_numbers_test.exs
@@ -0,0 +1,8 @@
+defmodule HoleNumbersTest do
+  use ExUnit.Case
+  doctest HoleNumbers
+
+  test "greets the world" do
+    assert HoleNumbers.hello() == :world
+  end
+end
